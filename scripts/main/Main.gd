ee041b27d16877838b2ed07b540b2b51464fb07b
cpt-miche <cptmiche@gmail.com>
2026-02-26 15:06:26 -0500

    Fix battle startup script errors

--- scripts/main/Main.gd
+++ scripts/main/Main.gd
@@ -1,7 +1,8 @@
 extends Node
 
 @onready var world: Node = $WorldIso
-@onready var battle: Node = $Battle
+@onready var battle: Control = $Battle
+@onready var battle_controller: Node = $Battle/BattleController
 
 var enemy_map := {
 	&"raditz_scout": preload("res://resources/fighters/raditz_scout.tres")
@@ -9,14 +10,14 @@ var enemy_map := {
 
 func _ready() -> void:
 	world.encounter_requested.connect(_on_encounter_requested)
-	battle.battle_finished.connect(_on_battle_finished)
+	battle_controller.battle_finished.connect(_on_battle_finished)
 	battle.visible = false
 
 func _on_encounter_requested(enemy_id: StringName) -> void:
 	if not enemy_map.has(enemy_id):
 		push_warning("Unknown enemy id: %s" % enemy_id)
 		return
-	battle.enemy_base = enemy_map[enemy_id]
+	battle_controller.enemy_base = enemy_map[enemy_id]
 	world.visible = false
 	battle.visible = true
 	battle.process_mode = Node.PROCESS_MODE_INHERIT
