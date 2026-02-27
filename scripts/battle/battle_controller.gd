101c07f49ba8ddd4ca41c143731de1486a6a3edc
cpt-miche <cptmiche@gmail.com>
2026-02-26 22:40:42 -0500

    Fix fighter resource parsing and Kaioken typing

--- scripts/battle/battle_controller.gd
+++ scripts/battle/battle_controller.gd
@@ -160,7 +160,7 @@ func _toggle_kaioken(fighter: FighterStats) -> void:
 	if fighter.form_level > 0:
 		_log("%s can only use Kaioken in base form." % fighter.fighter_name)
 		return
-	var kaioken := transformations.get(&"kaioken", null)
+	var kaioken: TransformationDef = transformations.get(&"kaioken", null)
 	if kaioken == null:
 		_log("Kaioken transformation is not configured.")
 		return
